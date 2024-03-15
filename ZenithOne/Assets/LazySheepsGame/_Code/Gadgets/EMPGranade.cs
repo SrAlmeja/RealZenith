a9d6a3b6f59b8e49d65eb4f6c13c6b506b67dad3
Fernando747 <fernandojcr5@gmail.com>
2024-03-15 03:06:57 -0600

    Fully working EMP granade

--- ZenithOne/Assets/LazySheepsGame/_Code/Gadgets/EMPGranade.cs
+++ ZenithOne/Assets/LazySheepsGame/_Code/Gadgets/EMPGranade.cs
@@ -1,25 +1,93 @@
 using com.LazyGames;
-using System.Collections;
-using System.Collections.Generic;
+using Lean.Pool;
+using NaughtyAttributes;
 using UnityEngine;
+using DG.Tweening;
 
 public class EMPGranade : MonoBehaviour
 {
     [Header("Dependencies")]
-    [SerializeField] private LayerMask _interactWithLayers;
+    [Required][SerializeField] private GameObject _topCover;
+    [Required][SerializeField] private GameObject _bottomCover;
+    [Required][SerializeField] private GameObject _explosionVFX;
+    [SerializeField] private float _vfxDuration = 1f;
 
+    [Header("Settings")]
+    [SerializeField] private LayerMask _interactWithLayers;
     [SerializeField] private TypeOfGadget _typeOfGadget;
+    [SerializeField] private float _interactionRadius = 1f;
+    [SerializeField] private float _maxDistance = 1f;
+    [SerializeField] private float _activationTime = 1f;
+    [SerializeField] private float _despawnTime = 1f;
+
+    private bool _isActive = false;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.layer == _interactWithLayers)
+        if (!_isActive) return;
+        if((_interactWithLayers.value & 1 << collision.gameObject.layer) != 0)
         {
-            ExplodeShpereCast();
+            ExplodeSphereCast();
+            FireVFX();
         }
     }
 
-    private void ExplodeShpereCast()
+
+    [Button("Activate Granade Test")]
+    public void ActivateGranade()
     {
-        //Physics.SphereCast
+        _topCover.transform.DOLocalMoveY(0.25f, _activationTime);
+        _bottomCover.transform.DOLocalMoveY(-0.25f, _activationTime);
+        _isActive = true;
     }
+
+    private void DeactivateGranade()
+    {
+        _topCover.transform.DOLocalMoveY(0f, 0f);
+        _bottomCover.transform.DOLocalMoveY(0f, 0f);
+        _isActive = false;
+    }
+
+    private void ExplodeSphereCast()
+    {
+        RaycastHit[] explosionHits = Physics.SphereCastAll(transform.position, _interactionRadius, Vector3.up, _maxDistance, _interactWithLayers);
+
+        foreach(RaycastHit hit in explosionHits)
+        {
+            IGadgetInteractable gadgetInteractable;
+
+            if(hit.collider.gameObject.TryGetComponent(out gadgetInteractable))
+            {
+                gadgetInteractable.GadgetInteraction(_typeOfGadget);
+            }
+        }
+    }
+
+    private void FireVFX()
+    {
+        _explosionVFX.transform.parent = null;
+        _explosionVFX.GetComponent<ParticleSystem>().Play(true);
+        Invoke("RestoreVFX", _vfxDuration);
+    }
+
+    private void RestoreVFX()
+    {
+        _explosionVFX.transform.parent = this.gameObject.transform;
+        _explosionVFX.transform.localPosition = Vector3.zero;
+        _explosionVFX.GetComponent<ParticleSystem>().Stop();
+        Invoke("Despawn", _despawnTime);
+    }
+
+    private void Despawn()
+    {
+        DeactivateGranade();
+        LeanPool.Despawn(this.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _interactionRadius);
+    }
+ 
 }
