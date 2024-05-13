e9abb53f60b8b302191c7b166916a14871337a9b
Angel Legorreta <cdmx1895@amerike.edu>
2024-05-13 10:14:00 -0500

    first Part of TransitionLevel

--- ZenithOne/Assets/LazySheepsGame/TheAlmejaFolder/Code/LayerSwitcher.cs
+++ ZenithOne/Assets/LazySheepsGame/TheAlmejaFolder/Code/LayerSwitcher.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 
 public class LayerSwitcher : MonoBehaviour
 {
-    public void TheSwitcher(GameObject selectedObject)
+    public void OnSelected(GameObject selectedObject)
     {
-        //Cambiar el DEFAULT_LAYER por el layer que contenga enemy o los que necesiten
-        if (selectedObject.layer != StaticLayer.SELECTED_SHADER_LAYER)
-        {
-            selectedObject.layer = StaticLayer.SELECTED_SHADER_LAYER;
-        }
-        else
-        {
-            selectedObject.layer = StaticLayer.DEFAULT_LAYER;
-        }
+        selectedObject.layer = StaticLayer.SELECTED_SHADER_LAYER;
+    }
+
+    public void OnDeselected(GameObject selectedObject)
+    {
+        selectedObject.layer = StaticLayer.DEFAULT_LAYER;
     }
 }
