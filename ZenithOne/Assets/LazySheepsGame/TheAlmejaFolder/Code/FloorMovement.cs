cb50890cfcda5897860ada662bae1d1f16a07321
LeinadDH <cdmx1831@amerike.edu.mx>
2024-05-17 15:58:23 -0600

    trying to Work

--- ZenithOne/Assets/LazySheepsGame/TheAlmejaFolder/Code/FloorMovement.cs
+++ ZenithOne/Assets/LazySheepsGame/TheAlmejaFolder/Code/FloorMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Lean.Pool;
 
 public class FloorMovement : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class FloorMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float aceleration;
     [SerializeField] private Vector3 direction;
+    [SerializeField] private Transform spawnZone;
 
     private void Awake()
     {
